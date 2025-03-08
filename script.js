63076d24d600f44d07cd38ba6369f777541c6108
pandaacolori <federicocredaro@gmail.com>
2025-03-08 14:51:32 +0100

    create grid

---
+++ script.js
@@ -0,0 +1,8 @@
+const gridContainer = document.getElementById('grid-container');
+
+for (let i = 0; i < 16 * 16; i++) {
+  const square = document.createElement('div')
+  square.classList.add('square');
+  gridContainer.appendChild(square); 
+}
+
