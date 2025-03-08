742d11d68cd2ef73dde0c5bd5fd4299cc820c944
pandaacolori <federicocredaro@gmail.com>
2025-03-08 15:13:22 +0100

    add functioning hover

--- script.js
+++ script.js
@@ -4,5 +4,12 @@ for (let i = 0; i < 16 * 16; i++) {
   const square = document.createElement('div')
   square.classList.add('square');
   gridContainer.appendChild(square); 
+  const hoveroo = document.querySelectorAll(".square")
+  hoveroo.forEach(div => {
+    div.addEventListener("mouseover", function() {
+    div.style.backgroundColor = "purple";
+});
+})
 }
+
 
