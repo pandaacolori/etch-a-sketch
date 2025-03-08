eea35f8d0e7f9cc231934eb48dc8684eb9691437
pandaacolori <federicocredaro@gmail.com>
2025-03-08 15:58:53 +0100

    reset button works but barely

--- script.js
+++ script.js
@@ -13,13 +13,32 @@ for (let i = 0; i < 16 * 16; i++) {
 }
 const btn = document.querySelector(".resetbtn");
     btn.addEventListener("click", () => {
-      const userInput = prompt("How many squares should the new grid have? (MAX 100)");
+      const userInput = prompt("How large should the new grid be? (max 16)");
       const numberInput = parseFloat(userInput);
         if (isNaN(numberInput)) {
+        alert("So close!! That is a shape, not a number <3")
         console.log("That's not a valid number.");
-  }     else {
+  }     
+        else if (numberInput >= 17) {
+        alert("That number is too damn high!");
+        console.log("That number is too damn high!");
+        }
+
+        else {
         console.log("User entered the number:", numberInput);
+        document.querySelectorAll('.square').forEach(e => e.remove());
+        for (let i = 0; i < numberInput * numberInput; i++) {
+          const square = document.createElement('div')
+          square.classList.add('square');
+          gridContainer.appendChild(square); 
+          const hoveroo = document.querySelectorAll(".square")
+          hoveroo.forEach(div => {
+            div.addEventListener("mouseover", function() {
+            div.style.backgroundColor = "purple";
+        });
+        })
+        }    
+
   }
 
   })
-
